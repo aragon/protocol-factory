b839e99a573204eba9ef582b6fa0ed6b708279e7
Jordan <45881807+jordaniza@users.noreply.github.com>
2025-06-04 14:16:35 +0400

    Pragma ^0.8.22 -> ^0.8.0

--- test/constants.sol
+++ test/constants.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.22;
+pragma solidity ^0.8.0;
 
 bytes32 constant EXECUTE_PERMISSION_ID = keccak256("EXECUTE_PERMISSION");
 bytes32 constant SET_METADATA_PERMISSION_ID = keccak256("SET_METADATA_PERMISSION");
